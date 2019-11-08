8ba1897123cb1f2239efee87c2e77670d39ba963
Mark Zuckerberg <zuck@facebook.com>
2019-11-08 13:08:59 +0100

    Change the status endpoint to /health
    To follow industry standards, the endpoint responsible for the status of
    the service should live under the /health endpoint and return a bool
    value denoted by the `health` key.

--- node-express-app.js
+++ node-express-app.js
@@ -30,8 +30,12 @@ var port = process.env.PORT || 8080;    // set our port
 
 var router = express.Router();        // get an instance of the express Router
 
-router.get('/', function(req, res) {
-  res.json({ up: true });
+router.get('/health', function(req, res) {
+  res.json({ health: true });
+});
+
+router.get('/hello', function(req, res) {
+  res.json({ answer: 42 });
 });
 
 app.use('/', router);
